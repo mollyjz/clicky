e3ac92406988e1e2a634cf763d70fa155de9e785
Molly Zurheide <you@example.com>
2018-11-18 16:19:49 -0500

    deploying

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
 
-import App from "./App";
+import App from "./App.js"; ////////////////
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
